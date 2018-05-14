a3c71a19a08b82c3df7e9801a5c42fa423473dc0
AndrZakhara <AndrZakhara@users.noreply.github.com>
2018-05-14 17:49:05 +0300

    component Search finished

---
+++ src/component/Search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './lib/css/fontawesome-all.min.css';
+import './lib/webfonts/fa-brands-400.eot';
+import './Search.css';
+
+export default function Search() {
+    const element =
+        <div className="c-search">
+            <form className="c-search__form"action="">
+                <input className="c-search__input"type="text" placeholder="Seach..."/>
+                <button className="c-search__button"type="submit">
+                    <i className="fas fa-search"></i>
+                </button>
+            </form>
+        </div>
+
+
+    return element;
+}
--- src/component/UserItemList.js
+++ src/component/UserItemList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import UserItem from './UserItem';
 import './UserItemList.css';
+import Search from './Search';
 
 export default function UserItemList(props) {
     const clients = props.clients;
     const userItemElements = clients.map((item) => <UserItem client = {item} />);
     const element = (
         <div className="c-user-list">
+            <Search />
             {userItemElements}
         </div>
     );
