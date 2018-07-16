147e455f69997083eadcb69f1d8254e169f1bc9d
Andriy Zakhar <art.pruttov@gmail.com>
2018-07-16 13:04:37 +0300

    02-react-color-rate-app: refs were added to AddColorForm

--- 02-react-color-rate-app/src/components/AddColorForm.js
+++ 02-react-color-rate-app/src/components/AddColorForm.js
@@ -1,27 +1,47 @@
 import React from 'react';
-import { component } from 'react';
+import { Component } from 'react';
 import '../styles/AddColorForm.css'
 
- const AddColorForm = () => (
+export default class AddColorForm extends Component {
+    constructor(props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+    submit(e) {
+        const { _title, _color } = this.refs
+        console.log(_title)
+        console.log(_color)
+        e.preventDefault();
+        alert(`New color: ${_title.value}: ${_color.value}`)
+        _title.value = '';
+        _color.value = '#000000';
+        _title.focus();
+    }
 
-    <header className="l-color-form">
-        <form className="c-color-form" onSubmit={e => e.preventDefault()}>
-            <input
-                className="c-color-form__text-input"
-                type="text"
-                placeholder="color title..."
-                required
-            />
-            <input
-                type="color"
-                // value="#7289da"
-            />
-            <input
-                type="button"
-                value="Add color"/>
-        </form>
-    </header>
 
-);
+    render() {
+        return (
+            <header className="l-color-form">
+                <form className="c-color-form" onSubmit={this.submit}>
+                    <input ref="_title"
+                        className="c-color-form__text-input"
+                        type="text"
+                        placeholder="color title..." required
+                    />
+                    <input ref = "_color"
+                        type="color"
+                    />
+                    <input
+                        type="submit"
+                        value="Add color"
+                    />
+                    {/*<button>Add color</button>*/}
+                </form>
+            </header>
+        )
+    }
+}
 
-export default AddColorForm
+
+
+
---
+++ 02-react-color-rate-app/src/components/AddMainLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddMainLayout () {
+    return (
+        <div className="l-main">
+
+        </div>
+    )
+}
+
+export default AddMainLayout
