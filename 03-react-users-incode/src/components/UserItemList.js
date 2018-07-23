12ed79f4fee39cc06be4e0c624b9a919d0e8006e
AndrZakhara <art.pruttov@gmail.com>
2018-07-23 19:50:50 +0300

    03-react-users-incode app: Change UserItemList

--- 03-react-users-incode/src/components/UserItemList.js
+++ 03-react-users-incode/src/components/UserItemList.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Component } from 'react';
 import UserSearch from './UserSearch';
 import UserItem from './UserItem';
 
-export default class UserItemList extends Component {
-    render() {
-        return (
-            <div className="c_user-info__user-item-list">
-                <UserSearch />
-                <UserItem />
-            </div>
-        )
-    }
+function UserItemList(props) {
+    const { userList } = props;
+
+    return (
+        <div className="c_user-info__user-item-list">
+            <UserSearch />
+            {userList.map((userInfo, i)=><UserItem userInfo={userInfo} key={i}/>)}
+        </div>
+    )
+
 }
+
+export default UserItemList
--- 03-react-users-incode/src/components/UsersInfoApp.js
+++ 03-react-users-incode/src/components/UsersInfoApp.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 import UserItemList from './UserItemList';
-import SelectedUserInfo from './SelectedUserInfo'
+import SelectedUserInfo from './SelectedUserInfo';
+import Data from '../clients.json';
 
 export default class UsersInfoApp extends Component {
     render() {
         return (
             <div className="c_user-info">
-                <UserItemList />
+                <UserItemList userList = {Data}/>
                 <SelectedUserInfo />
             </div>
         )
